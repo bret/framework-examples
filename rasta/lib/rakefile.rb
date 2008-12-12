5cf395f16186c8dcdbbdba2c53c36893de482329
Jim Matthews <jim_matt@swbell.net>
2008-12-12 14:39:25 -0600

    Add database reset to rakefile. Added a database reset to the rakefile to reset the depot_development database before each run of add_books.

--- rasta/lib/rakefile.rb
+++ rasta/lib/rakefile.rb
@@ -6,9 +6,18 @@ task :purchase_books do |t|
 end
 
 desc 'Run tests to add books to product list.'
-task :add_books do |t|
+task :add_books => :reset_db do |t|
  sh 'rasta Add_Book_to_product_list.xls -f fixtures'
 end
+
+task :reset_db do
+  Dir.chdir File.dirname(__FILE__) + '/../../../depot' do
+    unless system 'mysql -u root -ppassword depot_development < db\product_data.sql'
+      raise 'database error'
+    end
+  end
+end
+
 
 desc 'Run all tests.'
 task :all => [:purchase_books, :add_books]
