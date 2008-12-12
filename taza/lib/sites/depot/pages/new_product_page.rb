2472486180b8c44df45becd220d6836af8a318c9
Bret Pettichord <bret@pettichord.com>
2008-12-12 18:36:51 +0000

    work in progress on taza example working on the test to add a book to the catalog

---
+++ taza/lib/sites/depot/pages/new_product_page.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'taza/page'
+
+module Depot
+  class NewProductPage < ::Taza::Page
+    element(:title) {browser.text_field(:id, 'product_title')}
+    element(:description) {browser.text_field(:id, 'product_description')}
+    element(:image_url) {browser.text_field(:id, 'product_image_url')}
+    element(:price) {browser.text_field(:id, 'product_price')}
+  end
+end
