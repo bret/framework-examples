b6b5c2689d9f64d9921e48a2d3274bf1a567f4f2
Bret Pettichord <bret@pettichord.com>
2008-12-17 01:49:18 +0000

    remove depot/trunk, committed in error

--- depot/trunk/config/environments/development.rb
+++
@@ -1,9 +0,0 @@
-#---
-# Excerpted from "Agile Web Development with Rails"
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com for more book information.
-#---
-Dependencies.mechanism                             = :load
-ActionController::Base.consider_all_requests_local = true
-ActionController::Base.perform_caching             = false
-BREAKPOINT_SERVER_PORT = 42531
--- depot/trunk/db/drop.sql
+++
@@ -1,4 +0,0 @@
-drop table users;
-drop table line_items;
-drop table orders;
-drop table products;
