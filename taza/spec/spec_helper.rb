29a338a7bfff2a782bf93af206e5d694007a1736
Bret Pettichord <bret@pettichord.com>
2008-12-30 17:43:27 +0000

    TAZA_DEV can now be specified as an env var, and will be used if defined.

--- taza/spec/spec_helper.rb
+++ taza/spec/spec_helper.rb
@@ -3,8 +3,11 @@ require 'rubygems'
 require 'spec'
 require 'mocha'
 
-lib_path = File.expand_path("#{File.dirname(__FILE__)}/../lib/sites")
-$LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
+lib_paths = [File.expand_path("#{File.dirname(__FILE__)}/../lib/sites")]
+lib_paths << ENV['TAZA_DEV'] + 'lib' if ENV['TAZA_DEV']
+lib_paths.each do |lib_path|
+  $LOAD_PATH.unshift lib_path unless $LOAD_PATH.include?(lib_path)
+end
 
 Spec::Runner.configure do |config|
   config.mock_with :mocha
