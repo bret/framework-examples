584d28df87ba8908ecb38a5c6ef06235bc1b3df6
Alex Collins <alex@Alex.local>
2009-01-06 23:18:15 +0000

    Set the store application to be the default route so it can be accessed without the /store part of the URL

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -18,4 +18,6 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
+
+  map.connect '', :controller => 'store'
 end
