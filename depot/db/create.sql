d2fdb4f51b254c2dc8ca8d2f261c74b8c7cb58be
Bret Pettichord <bret@pettichord.com>
2009-01-06 16:41:06 +0000

    updated foriegn key

--- depot/db/create.sql
+++ depot/db/create.sql
@@ -4,36 +4,36 @@ drop table if exists orders;
 drop table if exists products;
 
 create table products (
- id              int           not null auto_increment,
- title           varchar(100)  not null,
+ id              int(11)       not null auto_increment,
+ title           varchar(100)  not null default '',
  description     text          not null,
- image_url       varchar(200)  not null,
- price           decimal(10,2) not null,
- date_available  datetime      not null,
+ image_url       varchar(200)  not null default '',
+ price           decimal(10,2) not null default '0.00',
+ date_available  datetime      not null default '0000-00-00 00:00:00',
  primary key (id)
 );
 
 /*START:orders*/
 create table orders (
- id              int           not null auto_increment,
+ id              int(11)       not null auto_increment,
  name            varchar(100)  not null,
  email           varchar(255)  not null,
  address         text          not null,
  pay_type        char(10)      not null,
- shipped_at	 datetime      null,
+ shipped_at	     datetime      null,
  primary key (id)
 );
 /*END:orders*/
 
 create table line_items (
-  id              int           not null auto_increment,
-  product_id	  int 	        not null,
-  order_id        int           not null,
-  quantity	  int           not null default 0,
-  unit_price	  decimal(10,2) not null,
-  constraint fk_items_product   foreign key (product_id) references products(id),
-  constraint fk_items_order     foreign key (order_id) references orders(id),
-  primary key (id)
+  id             int(11)       not null auto_increment,
+  product_id     int(11)       not null,
+  order_id       int(11)       not null,
+  quantity       int(11)       not null default 0,
+  unit_price     decimal(10,2) not null,
+  primary key (id),
+  KEY `fk_items_product` (`product_id`),
+  KEY `fk_items_order` (`order_id`)
 );
 
 
