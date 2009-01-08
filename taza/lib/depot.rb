92c4514f5ab32e3e9f3a67c0aa1f42e5a247501d
Bret Pettichord <bret@pettichord.com>
2009-01-08 20:30:31 +0000

    remove Depot.url, as it is now inherited from Site

--- taza/lib/depot.rb
+++ taza/lib/depot.rb
@@ -5,10 +5,6 @@ module Depot
   include ForwardInitialization
 
   class Depot < ::Taza::Site
-    def url
-      config = Settings.config(@class_name)
-      config[:url]
-    end   
     class << self
       def reset_database
         Dir.chdir File.dirname(__FILE__) + '/../../../depot' do
