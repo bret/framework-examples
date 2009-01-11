d9056b931a6f742c85fe52bfa0d301a26c8b7f14
Bret Pettichord <bret@pettichord.com>
2009-01-11 23:44:47 +0000

    use new Page.field method

--- taza/lib/pages/your_cart_page.rb
+++ taza/lib/pages/your_cart_page.rb
@@ -4,11 +4,6 @@ require 'taza/page'
 module Depot
   class YourCartPage < ::Taza::Page
     url 'store/display_cart'
-    element(:total_element) {@browser.cell(:id, 'totalcell')}
-    
-    def total
-      total_element.text
-    end
-
+    field(:total) {@browser.cell(:id, 'totalcell')}
   end
 end
