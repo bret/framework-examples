c66c640f4e1b27d870392a01e11c0b1243c44c59
Bret Pettichord <bret@pettichord.com>
2009-01-12 17:18:38 +0000

    update tests to use page fields

--- taza/lib/pages/new_product_page.rb
+++ taza/lib/pages/new_product_page.rb
@@ -3,10 +3,10 @@ require 'taza/page'
 
 module Depot
   class NewProductPage < ::Taza::Page
-    element(:title) {browser.text_field(:id, 'product_title')}
-    element(:description) {browser.text_field(:id, 'product_description')}
-    element(:image_url) {browser.text_field(:id, 'product_image_url')}
-    element(:price) {browser.text_field(:id, 'product_price')}
+    field(:title) {browser.text_field(:id, 'product_title')}
+    field(:description) {browser.text_field(:id, 'product_description')}
+    field(:image_url) {browser.text_field(:id, 'product_image_url')}
+    field(:price) {browser.text_field(:id, 'product_price')}
     element(:create_button) {browser.button(:value, 'Create')}
   end
 end
--- taza/test/specs/add_book_spec.rb
+++ taza/test/specs/add_book_spec.rb
@@ -11,10 +11,10 @@ describe "Add Book" do
       # not sure how to do this with reference to url in depot.yml
       depot.browser.goto 'http://localhost:3000/admin/new'
       depot.new_product_page do |page|
-        page.title.set 'The Tycoons'
-        page.description.set 'How Andrew Carnegie, John D. Rockefeller, Jay Gould, and J. P. Morgan Invented the American Supereconomy'
-        page.image_url.set 'http://ecx.images-amazon.com/images/I/51qobzUJkdL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA240_SH20_OU01_.jpg'
-        page.price.set '16.00'
+        page.title = 'The Tycoons'
+        page.description = 'How Andrew Carnegie, John D. Rockefeller, Jay Gould, and J. P. Morgan Invented the American Supereconomy'
+        page.image_url = 'http://ecx.images-amazon.com/images/I/51qobzUJkdL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA240_SH20_OU01_.jpg'
+        page.price = '16.00'
         page.create_button.click
       end
     end
