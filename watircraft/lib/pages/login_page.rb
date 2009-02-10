e02034c262b069ee12918504f49bba7d480b601b
Bret Pettichord <bret@pettichord.com>
2009-02-10 23:09:56 +0000

    whitespace

--- watircraft/lib/pages/login_page.rb
+++ watircraft/lib/pages/login_page.rb
@@ -4,8 +4,8 @@ require 'taza/page'
 module Depot
   class LoginPage < ::Taza::Page
     url 'login/login'
-    field(:user_name) {browser.text_field(:id, 'user_name')}
-    field(:password) {browser.text_field(:id, 'user_password')}
-    element(:login_button) {browser.button(:value, ' LOGIN ')}
+    field(:user_name) { browser.text_field(:id, 'user_name') }
+    field(:password) { browser.text_field(:id, 'user_password') }
+    element(:login_button) { browser.button(:value, ' LOGIN ') }
   end
 end
