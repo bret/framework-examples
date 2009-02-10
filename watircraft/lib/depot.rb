5d24ecd3faea4b7a8e14ddae305d03a99e7e9acb
Bret Pettichord <bret@pettichord.com>
2009-02-10 23:08:49 +0000

    improve code readability

--- watircraft/lib/depot.rb
+++ watircraft/lib/depot.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-$LOAD_PATH.unshift File.expand_path(ENV['TAZA_DEV'] + '/lib') if ENV['TAZA_DEV']
 require 'taza'
 
 module Depot
@@ -9,12 +8,10 @@ module Depot
     def page_heading
       @browser.div(:id, 'banner').text
     end
-    class << self
-      def reset_database
-        Dir.chdir File.dirname(__FILE__) + '/../../../depot' do
-          unless system 'mysql -u root -ppassword depot_development < db\product_data.sql'
-            raise 'database error'
-          end
+    def self.reset_database
+      Dir.chdir File.dirname(__FILE__) + '/../../../depot' do
+        unless system 'mysql -u root -ppassword depot_development < db\product_data.sql'
+          raise 'database error'
         end
       end
     end
