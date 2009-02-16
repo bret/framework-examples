db720a56f882665dfcb45c8e41cc1eb66fedf547
Bret Pettichord <bret@pettichord.com>
2009-02-16 04:46:27 +0000

    reformat

--- watircraft/test/specs/add_book_spec.rb
+++ watircraft/test/specs/add_book_spec.rb
@@ -1,36 +1,34 @@
 $LOAD_PATH.unshift File.dirname(__FILE__) unless 
-  $LOAD_PATH.include? File.dirname(__FILE__)
+$LOAD_PATH.include? File.dirname(__FILE__)
 require 'spec_helper'
 
 describe "Add Book" do
   it "should allow a book to be added to the catalog" do
-    @site.reset_database
+    site.reset_database
     login_page do |page|
       page.goto
       page.user_name = 'dave'
       page.password = 'secret'
-        page.login_button.click
-      end
- 
-      tycoons = {
-        :title => 'The Tycoons',
+      page.login_button.click
+    end
+    
+    tycoons = {
+      :title => 'The Tycoons',
       :description => '<P>How Andrew Carnegie, John D. Rockefeller, Jay Gould, and J. P. Morgan Invented the American Supereconomy</P>',
       :image_url => 'http://ecx.images-amazon.com/images/I/51qobzUJkdL._BO2,204,203,200_PIsitb-sticker-arrow-click,TopRight,35,-76_AA240_SH20_OU01_.jpg',
       :price => '16.0' 
     }
- 
-      new_product_page do |page|
-        page.goto
-        page.populate tycoons
-        page.create_button.click
-      end
-      
-      browser.url.should == product_listing_page.full_url
-      product_listing_page.show_link('The Tycoons').click
-
-    show_product_page do |page|
-      browser.url.should match(Regexp.new(page.full_url))
-      page.validate tycoons
+    
+    new_product_page do |page|
+      page.goto
+      page.populate tycoons
+      page.create_button.click
     end
+    
+    browser.url.should == product_listing_page.full_url
+    product_listing_page.show_link('The Tycoons').click
+    
+    browser.url.should match(/#{show_product_page.full_url}/)
+    show_product_page.validate tycoons
   end
 end
